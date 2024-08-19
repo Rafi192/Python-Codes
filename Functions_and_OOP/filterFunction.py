70b02a528b793249149b0e65ee4bd319f1fca394
Rafi Hasan <rafihasanmmsc@gmail.com>
2024-08-20 00:13:28 +0600

    Filter method in python

---
+++ Functions_and_OOP/filterFunction.py
@@ -0,0 +1,9 @@
+def filter_even(num):
+    return num %2 ==0
+
+myNums = [1,2,3,4,5,6,7,8,9,10]
+print(list(filter(filter_even,myNums)))
+# the filter method takes a funtion and an iterable 
+# and returns the filtered values based on the functions logic
+
+#print(list(map(filter_even,myNums)))
